9be8c39501233f2bd65339e9661f41b68e964d1b
JAEHO SONG <thruthesky@gmail.com>
2021-03-16 17:11:13 +0900

    button update

--- lib/simple_button.dart
+++ lib/simple_button.dart
@@ -1,17 +1,23 @@
 library button;
 
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Button extends StatefulWidget {
   Button({
     this.alignment,
-    @required this.child,
-    this.margin = const EdgeInsets.all(0.0),
-    this.padding = const EdgeInsets.all(8.0),
     this.backgroundColor = const Color.fromARGB(255, 222, 222, 222),
-    @required this.onPressed,
+    @required this.child,
     this.elevation = 0.0,
+    this.loader = false,
+    this.margin = const EdgeInsets.all(0.0),
+    @required this.onPressed,
+    this.padding = const EdgeInsets.all(8.0),
     this.radius = 0.0,
+    this.spinnerColor = Colors.black,
+    this.spinnerSize = 16.0,
   });
   final Alignment alignment;
   final Widget child;
@@ -21,6 +27,11 @@ class Button extends StatefulWidget {
   final Function onPressed;
   final double elevation;
   final double radius;
+  final bool loader;
+
+  /// The [spinnerColor] works only for Android. For iOS, you may change theme data.
+  final Color spinnerColor;
+  final double spinnerSize;
   @override
   _ButtonState createState() => _ButtonState();
 }
@@ -35,7 +46,17 @@ class _ButtonState extends State<Button> {
           borderRadius: BorderRadius.circular(widget.radius),
           child: Container(
             alignment: widget.alignment,
-            child: widget.child,
+            child: widget.loader
+                ? SizedBox(
+                    width: widget.spinnerSize,
+                    height: widget.spinnerSize,
+                    child: Platform.isAndroid
+                        ? CircularProgressIndicator(
+                            valueColor: AlwaysStoppedAnimation<Color>(widget.spinnerColor),
+                          )
+                        : CupertinoActivityIndicator(),
+                  )
+                : widget.child,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(widget.radius),
               color: widget.backgroundColor,
